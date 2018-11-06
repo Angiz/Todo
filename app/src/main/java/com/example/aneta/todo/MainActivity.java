ceb8039338dd646b600d42e2ffaba83dd5e7c1ef
Aneta <angiziewska@gmail.com>
2018-11-06 16:29:49 +0100

    my first commit - toolbar and items

---
+++ app/src/main/java/com/example/aneta/todo/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.aneta.todo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (item.getItemId()) {
+            case R.id.dodaj:
+                Snackbar.make(getWindow().getDecorView(),"Dodano zadanie",Snackbar.LENGTH_SHORT).setAction("Action", null).show();
+                return true;
+            case R.id.usun:
+                Toast.makeText(this,"Usunieto zadanie",Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
